8ee6acabffe3bf264b912a58a7efc6c46a36b3b2
woominsik <9737638@naver.com>
2022-07-26 16:27:23 +0900

    getLongPathValueByIndex 도입하여, 상세페이지에 적절한 게시물 노출

--- src/main/java/com/ll/exam/Rq.java
+++ src/main/java/com/ll/exam/Rq.java
@@ -85,4 +85,29 @@ public class Rq {
     public String getMethod() {
         return req.getMethod();
     }
+
+    public long getLongPathValueByIndex(int index, long defaultValue) {
+        String value = getPathValueByIndex(index, null);
+
+        if ( value == null ) {
+            return defaultValue;
+        }
+
+        try {
+            return Long.parseLong(value);
+        }
+        catch ( NumberFormatException e ) {
+            return defaultValue;
+        }
+    }
+
+    public String getPathValueByIndex(int index, String defaultValue) {
+        String[] bits = req.getRequestURI().split("/");
+
+        try {
+            return bits[4 + index];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return defaultValue;
+        }
+    }
 }
